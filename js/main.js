d9ca2c7d5773222cbb6c4e796ea6f513dbfcfd10
DrkManix <45601260+DrkManix@users.noreply.github.com>
2020-04-21 01:40:49 +0200

    NEWCODE

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+  // add function to all info buttons
+  $('.info-button').click(function(){
+    // add open class to button
+    $(this).toggleClass("open");
+    // get next element, which is description
+    var description = $(this).next();
+    if ( description.height() ) {
+      // description is open, we want to close it!
+      // so: we delete the max-height attribute
+      description.css('max-height', '0');
+    } else {
+      // description is closed, we want to open it!
+      // scrollHeight is the height of the object, regardless of whether it's displayed or not
+      description.css('max-height', description.prop('scrollHeight') + "px");
+    }
+  });
+  // add accordion to menu
+  $('.menu-button').click(function(){
+    // add open class to button
+    $(this).toggleClass("open");
+    // get next element, which is description
+    var subnav = $(this).next();
+    if ( subnav.height() ) {
+      // description is open, we want to close it!
+      // so: we delete the max-height attribute
+      subnav.css('max-height', '0');
+    } else {
+      // description is closed, we want to open it!
+      // scrollHeight is the height of the object, regardless of whether it's displayed or not
+      subnav.css('max-height', subnav.prop('scrollHeight') + "px");
+    }
+  });
+  document.getElementById('undo').style.backgroundImage="url(img/Work/UNDO/UNDO_BG.jpg)";
+
+});
